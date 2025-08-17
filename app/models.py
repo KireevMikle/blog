443028f64543311abaa72eb1e0e79f78dfb436bb
Kireev Mikle <kireev.mikle@gmail.com>
2025-08-17 12:34:09 +0100

    update1

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Post (models.Model):
+    title = models.CharField(max_length=100)
+    content= models.TextField(max_length=100)
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    
